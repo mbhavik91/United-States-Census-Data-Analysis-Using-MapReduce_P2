867fa4ffe84bd57c43abdef4caf0d80d0df7cc8b
Bhavik Mistry <mbhavik91@users.noreply.github.com>
2016-07-25 19:27:47 -0500

    Update MyPartitioner.java

--- src/cs455/hadoop/part2/MyPartitioner.java
+++ src/cs455/hadoop/part2/MyPartitioner.java
@@ -15,7 +15,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
  */
 public class MyPartitioner extends Partitioner<Text, CustomDataTypeSetter>{
 
-	public static String[] statesAbbreviation = {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "VI","DC"};
+    public static String[] statesAbbreviation = {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "VI","DC"};
     public static ArrayList<String> statesName = new ArrayList<String>(Arrays.asList(statesAbbreviation));
 	@Override
 	public int getPartition(Text key, CustomDataTypeSetter val, int arg2) {
